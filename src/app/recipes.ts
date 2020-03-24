74bb863cebf794852f66f35fe52bbeee388c5b50
syh7 <sjoerd0707@live.nl>
2020-03-24 17:25:59 +0100

    Move recipes.ts

---
+++ src/app/recipes.ts
@@ -0,0 +1,37 @@
+export const recipes = [
+    {
+        name : 'Spaghetti rode saus',
+        category : 'pasta',
+        vegan : true,
+        quick : false,
+        description : 'pasta/pastasaus/ui/knoflook/prei/wortel/selderij/paprika/geraspte kaas'
+    },
+    {
+        name : 'Pastasalade',
+        category : 'pasta',
+        vegan : true,
+        quick : false,
+        description : 'pasta/avocado/ijsbergsla/paprika/pesto/mozzarella/cherrytomaatjes/(spekjes)'
+    },
+    {
+        name : 'Roerbakgroente met rijst',
+        category : 'rijst',
+        vegan : true,
+        quick : true,
+        description : 'rijst/ui/knoflook/paprika/prei/taugé/wortel/nep-kip'
+    },
+    {
+        name : 'Chili',
+        category : 'overig',
+        vegan : true,
+        quick : false,
+        description : ''
+    },
+    {
+        name : 'Couscoussalade',
+        category : 'overig',
+        vegan : true,
+        quick : false,
+        description : 'Couscous/paprika/courgette/tomaatjes/(gerookte kip)'
+    }
+]
