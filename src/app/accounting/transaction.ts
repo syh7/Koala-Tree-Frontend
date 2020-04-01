c72a8d76364d4a559fdd62e7c673b71c243c32da
syh7 <sjoerd0707@live.nl>
2020-04-01 14:48:21 +0200

    Basic transaction object

---
+++ src/app/accounting/transaction.ts
@@ -0,0 +1,11 @@
+export interface Transaction {
+    id: number;
+    amountTotal: number;
+    amountSjoerd: number;
+    amountLoes: number;
+    amountAll: number;
+    category: string;
+    message: string;
+    date: Date;
+    createdDateTime: Date;
+}
