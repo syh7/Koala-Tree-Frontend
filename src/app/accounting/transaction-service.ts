6311987b129bb2cb0e0c4615cda70f84a38a0d16
syh7 <sjoerd0707@live.nl>
2020-04-07 15:11:23 +0200

    Use correct LoggerService instead of incorrect Logger

--- src/app/accounting/transaction-service.ts
+++ src/app/accounting/transaction-service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
 
 import { Transaction } from './transaction';
-import { Logger } from '../commons/logger/logger';
+import { LoggerService } from '../commons/logger/logger-service';
 
 @Injectable({
     providedIn: 'root'
@@ -12,15 +12,15 @@ export class TransactionService {
     baseUrl = 'http://localhost:8081/api/';
     transactionUrl = this.baseUrl + 'transactions/';
     
-    constructor(private http: HttpClient, private logger : Logger){ }
+    constructor(private http: HttpClient, private loggerService : LoggerService){ }
 
     getAllTransactions() : Observable<Transaction[]> {
-        this.logger.log("Request all transactions");
+        this.loggerService.log("Request all transactions");
         return this.http.get<Transaction[]>(this.transactionUrl);
     }
 
     getTransactionById(id : number) : Observable<Transaction> {
-        this.logger.log("Request transaction " + id);
+        this.loggerService.log("Request transaction " + id);
         return this.http.get<Transaction>(this.transactionUrl + id);
     }
 }
