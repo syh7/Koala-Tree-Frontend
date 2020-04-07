b6bb3ba7752e51670a5b06c543f2c60524b90c2e
syh7 <sjoerd0707@live.nl>
2020-04-07 15:07:38 +0200

    Add loggerService

--- src/app/accounting/transaction-service.ts
+++ src/app/accounting/transaction-service.ts
@@ -1,21 +1,26 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
 
 import { Transaction } from './transaction';
+import { Logger } from '../commons/logger/logger';
 
-@Injectable()
+@Injectable({
+    providedIn: 'root'
+})
 export class TransactionService {
-    baseUrl = 'localhost:8081/api/';
+    baseUrl = 'http://localhost:8081/api/';
     transactionUrl = this.baseUrl + 'transactions/';
     
-    constructor(private http: HttpClient){ }
+    constructor(private http: HttpClient, private logger : Logger){ }
 
     getAllTransactions() : Observable<Transaction[]> {
+        this.logger.log("Request all transactions");
         return this.http.get<Transaction[]>(this.transactionUrl);
     }
 
     getTransactionById(id : number) : Observable<Transaction> {
+        this.logger.log("Request transaction " + id);
         return this.http.get<Transaction>(this.transactionUrl + id);
     }
 }
