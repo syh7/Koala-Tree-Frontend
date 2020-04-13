294941c1a86ad2e9ff9abb8f115d82527b9a6793
syh7 <sjoerd0707@live.nl>
2020-04-13 17:38:16 +0200

    Add component to declarations

--- src/app/recipe/recipes.module.ts
+++ src/app/recipe/recipes.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RecipeRoutingModule } from './recipes-routing.module';
 import { CommonModule } from '@angular/common';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 
 @NgModule({
     imports:[
         CommonModule,
         RecipeRoutingModule
+    ],
+    declarations:[
+        RecipeListComponent
     ]
 })
 export class RecipesModule{}
